2f86721903799c1babde4cd8d539d883108cd905
Marcos Leite <m.abner@live.co.uk>
2022-08-17 01:14:19 -0300

    tailwindcss properly set

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Testimonials from './components/Testimonials';
+import './App.css';
 
 export default function App() {
 	return (
 		<main>
+			<div className='flex justify-center'>
+				<h1 className='font-bold text-2xl text-blue-900'>
+					React and Tailwind with Vitejs!
+				</h1>
+			</div>
 			<Navbar />
 			<About />
 			<Projects />
