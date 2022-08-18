0c48fd58549d9a8114db6067978c751ef4ba9861
Marcos Leite <m.abner@live.co.uk>
2022-08-18 03:26:57 -0300

    vite reconfig to default directories

--- vite.config.cjs
+++ vite.config.cjs
@@ -6,10 +6,3 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 });
-module.exports = {
-	root: './',
-	build: {
-		outDir: 'dist',
-	},
-	publicDir: 'assets',
-};
