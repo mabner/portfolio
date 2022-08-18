c522552c018ed1c2c230f3a2f3c02453c4a10da0
Marcos Leite <m.abner@live.co.uk>
2022-08-18 02:38:47 -0300

    creates contact form (netlify forms)

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,14 @@ import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Testimonials from './components/Testimonials';
-import './App.css';
 
 export default function App() {
 	return (
-		<main>
-			<div className='flex justify-center'>
-				<h1 className='font-bold text-2xl text-blue-900'>
-					React and Tailwind with Vitejs!
-				</h1>
-			</div>
+		<main className='text-gray-400 bg-gray-900 body-font'>
 			<Navbar />
 			<About />
 			<Projects />
 			<Skills />
-			<Testimonials />
 			<Contact />
 		</main>
 	);
