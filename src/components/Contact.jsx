32a95c326743d0c1ccd737fe0ef3a738e716268e
Marcos Leite <30819867+mabner@users.noreply.github.com>
2023-06-23 02:12:50 -0300

    adds captcha to contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,7 @@ export default function Contact() {
 	return (
 		<section id='contact' className='relative'>
 			<div className='container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap'>
-				<form name='contact' netlify-honeypot='bot-field' data-netlify='true' onSubmit={handleSubmit}>
+				<form name='contact' netlify-honeypot='bot-field' data-netlify='true' data-netlify-recaptcha='true' onSubmit={handleSubmit}>
 					<h2 className='text-white sm:text-4xl text-3xl mb-1 font-medium title-font'>
 						Contato
 					</h2>
@@ -75,6 +75,7 @@ export default function Contact() {
 							onChange={(e) => setMessage(e.target.value)}
 						/>
 					</div>
+					</p><div data-netlify-recaptcha='true'></div><p>
 					<button
 						type='submit'
 						className='text-white bg-indigo-500 border-0 py-2 px-10 focus:outline-none hover:bg-indigo-600 rounded text-lg'
