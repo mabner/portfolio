6a80db050ef7002c517e794837d5131d0e3eac07
Marcos Leite <m.abner@live.co.uk>
2023-09-01 03:01:21 -0300

    removes redundant button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,12 +22,6 @@ export default function About() {
 						>
 							Entre em contato
 						</a>
-						<a
-							href='#professional'
-							className='ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg'
-						>
-							Experiencia Profissional
-						</a>
 					</div>
 				</div>
 				<div className='lg:max-w-lg lg:w-1/3 md:w-1/2 w-5/6'>
