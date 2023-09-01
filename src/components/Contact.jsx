cce4e96cc290b770f5ec3963ffb656bb6695f607
Marcos Leite <m.abner@live.co.uk>
2023-09-01 03:25:42 -0300

    google recaptcha: moves div and param to jsx file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,6 +30,7 @@ export default function Contact() {
 				<form
 					name='contact'
 					netlify-honeypot='bot-field'
+					data-netlify-recaptcha="true"
 					data-netlify='true'
 					onSubmit={handleSubmit}
 				>
@@ -41,6 +42,11 @@ export default function Contact() {
 							Don’t fill this out if you’re human: <input name='bot-field' />
 						</label>
 					</p>
+					<div
+						class="g-recaptcha"
+						data-sitekey="6Ldbse4nAAAAAO-Ij6R5KKxz0KTVnsHOzUO8TGvk"
+						data-theme="dark"
+					></div>
 					<p className='leading-relaxed mb-5'>
 						Caso deseje entrar em contato basta preencher o formulário abaixo.
 					</p>
